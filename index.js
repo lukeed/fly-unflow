5f3c6fa02f90582d8808dab7fd792adc2d127fe8
Luke Edwards <luke.edwards05@gmail.com>
2017-04-17 00:49:00 -0700

    add sourceMap support

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const clean = require('flow-remove-types');
 
-module.exports = function (fly, utils) {
-  fly.plugin('unflow', {every: false}, function * (files, opts) {
-  	opts = Object.assign({pretty: true, all: true}, opts);
+module.exports = function (fly) {
+	fly.plugin('unflow', {every: false}, function * (files, opts) {
+		opts = Object.assign({pretty: true, all: true}, opts);
 
-  	files.forEach(file => {
-	  	const out = clean(file.data.toString(), opts);
-	  	file.data = Buffer.from(out.toString());
-  	});
-  });
+		const type = opts.sourceMap || false;
+		const maps = [];
+
+		files.forEach(file => {
+			const out = clean(file.data.toString(), opts);
+			file.data = Buffer.from(out.toString());
+
+			const mdata = Buffer.from(type ? JSON.stringify(out.generateMap()) : '');
+
+			// handle sourcemaps
+			if (type === 'inline') {
+				file.data += Buffer.from(`\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,${mdata.toString('base64')}`);
+			} else if (type === 'external') {
+				const mfile = `${file.base}.map`;
+				file.data += Buffer.from(`\n//# sourceMappingURL=${mfile}`);
+				// push an external sourcemap to output
+				maps.push({
+					dir: file.dir,
+					base: mfile,
+					data: mdata
+				});
+			}
+		});
+
+		if (maps.length > 0) {
+			this._.files = this._.files.concat(maps);
+		}
+	});
 };
